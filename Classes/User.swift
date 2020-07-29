684835e0aebb75d11efefce84e8539e2bcb8a4f7
johnstockton <johnstockton@johnstocktondeMacBook-Air.local>
2020-07-29 21:27:49 +0800

    modify class folder

--- Classes/User.swift
+++ Classes/User.swift
@@ -1,3 +1,17 @@
-import Foundation
 
-print("123")
+
+class User{
+    var name:String
+    
+    init(){
+        self.name = "init"
+    }
+    
+    func setName(mingzi: String){
+        self.name = mingzi
+    }
+    
+    func getName() -> String {
+        return self.name
+    }
+}
